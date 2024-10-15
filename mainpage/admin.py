862909cf96b21c8f3e6cd168075904410760c3e9
omurkanovd22 <omurkanovd22@gmail.com>
2024-10-15 13:12:42 +0600

    create AdditionalInfo table

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from mainpage.models import PersonalInfo
+from mainpage.models import PersonalInfo, AdditionalInfo
 
 
 @admin.register(PersonalInfo)
 class PersonalInfoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(AdditionalInfo)
+class AdditionalInfoAdmin(admin.ModelAdmin):
     pass
--- mainpage/models.py
+++ mainpage/models.py
@@ -4,15 +4,15 @@ from ckeditor.fields import RichTextField
 
 class PersonalInfo(models.Model):
     fullname = models.CharField("Полное имя", max_length=255)
-    email = models.CharField("Почта", max_length=255, default="")
-    phone_number = models.CharField("Номер телефона", max_length=255, default="")
-    linked_in = models.CharField("Ссылка на LinkedIn", max_length=255, default="")
-    github = models.CharField("Ссылка на GitHub", max_length=255, default="")
     profession = models.CharField("Профессия", max_length=255)
-    resume_file = models.FileField("Файл резюме", upload_to="perosnal")
-    about_me = RichTextField("Текст обо мне")
     image = models.ImageField("Картинка баннера", upload_to="personal", null=True)
     image_mobile = models.ImageField("Картинка баннера для мобилки", upload_to="personal", null=True)
+    resume_file = models.FileField("Файл резюме", upload_to="perosnal")
+    about_me = RichTextField("Текст обо мне")
+    linked_in = models.CharField("Ссылка на LinkedIn", max_length=255, default="")
+    github = models.CharField("Ссылка на GitHub", max_length=255, default="")
+    email = models.CharField("Почта", max_length=255, default="")
+    phone_number = models.CharField("Номер телефона", max_length=255, default="")
 
     class Meta:
         verbose_name = "Персональная информальная"
@@ -20,3 +20,15 @@ class PersonalInfo(models.Model):
 
     def __str__(self):
         return self.fullname
+
+class AdditionalInfo(models.Model):
+    icon = models.ImageField("Иконка", upload_to="additional")
+    title = models.CharField("Заголовок", max_length=50)
+    text = RichTextField("Текст")
+
+    class Meta:
+        verbose_name = "Дополнительная информация"
+        verbose_name_plural = "Дополнительная информация"
+
+    def __str__(self):
+        return self.title
